75c75810e24faaeb872e8c68995c875a44d74764
N_Da <nadejdadawydowa25@gmail.com>
2025-07-18 18:51:08 +0300

    add register/

---
+++ backend/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from backend.models import User, Product, ProductInfo, Shop, Category, Parameter, ProductParameter
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'username', 'password']
+    
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            email=validated_data['email'],
+            password=validated_data['password'],
+            username=validated_data.get('username', '')
+        )
+        return user
