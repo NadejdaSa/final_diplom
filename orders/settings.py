b3666ce3211783119c34f77ec43bdf60dbac1c6e
N_Da <nadejdadawydowa25@gmail.com>
2025-07-18 11:29:25 +0300

    delete db sclite

--- orders/settings.py
+++ orders/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'orders.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'diplom_db',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
