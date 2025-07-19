8f6dfe111f6c36bc1490192b0cd820ea6209be14
N_Da <nadejdadawydowa25@gmail.com>
2025-07-19 22:14:12 +0300

    add urls.py and views.py

--- backend/urls.py
+++ backend/urls.py
@@ -1,7 +1,19 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from backend.views import *
 
-from backend.views import RegisterView
+router = DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'shops', ShopViewSet)
+router.register(r'categories', CategoryViewSet)
+router.register(r'products', ProductViewSet)
+router.register(r'product_info', ProductInfoViewSet)
+router.register(r'parameters', ParameterViewSet)
+router.register(r'product_parameters', ProductParameterViewSet)
+router.register(r'order_items', OrderItemViewSet)
+router.register(r'orders', OrderViewSet)
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
+    path('', include(router.urls)),
 ]
--- backend/views.py
+++ backend/views.py
@@ -1,10 +1,55 @@
 from django.shortcuts import render
-from rest_framework import generics
-from .serializers import UserSerializer
-from .models import User
+from rest_framework import generics, viewsets
+from .serializers import CategorySerializer, OrderItemSerializer, OrderSerializer, ParameterSerializer, ProductInfoSerializer, ProductParameterSerializer, ProductSerializer, ShopSerializer, UserSerializer
+from .models import Category, Order, OrderItem, Parameter, Product, ProductInfo, ProductParameter, Shop, User
 
 
 # Create your views here.
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class ShopViewSet(viewsets.ModelViewSet):
+    queryset = Shop.objects.all()
+    serializer_class = ShopSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductInfoViewSet(viewsets.ModelViewSet):
+    queryset = ProductInfo.objects.all()
+    serializer_class = ProductInfoSerializer
+
+
+class ParameterViewSet(viewsets.ModelViewSet):
+    queryset = Parameter.objects.all()
+    serializer_class = ParameterSerializer
+
+
+class ProductParameterViewSet(viewsets.ModelViewSet):
+    queryset = ProductParameter.objects.all()
+    serializer_class = ProductParameterSerializer
+
+
+class OrderItemViewSet(viewsets.ModelViewSet):
+    queryset = OrderItem.objects.all()
+    serializer_class = OrderItemSerializer
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
