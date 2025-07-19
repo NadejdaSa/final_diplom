94c0b810150d3bdd90bee15a4736a41acf91c44c
N_Da <nadejdadawydowa25@gmail.com>
2025-07-19 21:43:25 +0300

    improved serializers.py

--- backend/serializers.py
+++ backend/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from backend.models import User, Product, ProductInfo, Shop, Category, Parameter, ProductParameter
+from backend.models import Order, OrderItem, User, Product, ProductInfo, Shop, Category, Parameter, ProductParameter
+
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -7,7 +8,8 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'email', 'username', 'password']
-    
+        extra_kwargs = {'password': {'write_only': True}}
+
     def create(self, validated_data):
         user = User.objects.create_user(
             email=validated_data['email'],
@@ -15,3 +17,66 @@ class UserSerializer(serializers.ModelSerializer):
             username=validated_data.get('username', '')
         )
         return user
+
+
+class ShopSerializer(serializers.ModelSerializer):
+    categories = serializers.StringRelatedField(many=True, read_only=True)
+
+    class Meta:
+        model = Shop
+        fields = ['id', 'name', 'url', 'categories']
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    shops = ShopSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Category
+        fields = ['id', 'name']
+
+
+class ProductSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'category']
+
+
+class ProductInfoSerializer(serializers.ModelSerializer):
+    product = serializers.PrimaryKeyRelatedField(queryset=Product.objects.all())
+    shop = serializers.PrimaryKeyRelatedField(queryset=Shop.objects.all())
+
+    class Meta:
+        model = ProductInfo
+        fields = ['id', 'product', 'shop', 'model', 'quantity', 'price', 'price_rrc']
+
+
+class ParameterSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Parameter
+        fields = ['id', 'name']
+
+
+class ProductParameterSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ProductParameter
+        fields = ['id', 'product_info', 'parameter', 'value']
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    product = ProductInfoSerializer(read_only=True)
+
+    class Meta:
+        model = OrderItem
+        fields = ['id', 'product', 'shop', 'quantity']
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True, read_only=True)
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Order
+        fields = ['id', 'user', 'dt', 'status', 'items']
--- orders/settings.py
+++ orders/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'djangorestframework',
+    'rest_framework',
     'rest_framework.authtoken',
     'django_rest_passwordreset',
     'backend',
