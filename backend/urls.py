87139a6233e645bf345599ed635930cc2b18b753
N_Da <nadejdadawydowa25@gmail.com>
2025-07-21 18:19:35 +0300

    improved urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -12,9 +12,13 @@ router.register(r'parameters', ParameterViewSet)
 router.register(r'product_parameters', ProductParameterViewSet)
 router.register(r'order_items', OrderItemViewSet)
 router.register(r'orders', OrderViewSet)
-router.register(r'contacts', ContactViewSet)
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('', include(router.urls)),
+    path('partner/update/', PartnerUpdate.as_view(), name='partner_update'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('products/', ProductView.as_view(), name='products'),
+    path('basket/', BasketView.as_view(), name='basket'),
+    path('contacts/', ContactView.as_view(), name='contacts'),
 ]
